8f596fe8fa923cb1107eed536d3feb0c011e8a78
csterritt <csterritt@gmail.com>
2013-10-04 09:14:14 -0400

    Fixed ordering bug.
    Added two more objects to the demo, so you can see that reordering
    by moving something up from below works, but moving the top element
    down two winds up with the first two items having the same 'idx'.
    This is because Ember sorts the list on every object modification
    by default, so elements are moved before they're reindexed.  Adding
    the calls to "beginPropertyChanges" and "endPropertyChanges" makes
    it work correctly in all cases.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ App.Router.map(function() {
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return [
-      Ember.Object.create({id: '1', idx: 2}),
+      Ember.Object.create({id: '1', idx: 4}),
+      Ember.Object.create({id: '4', idx: 3}),
+      Ember.Object.create({id: '5', idx: 2}),
       Ember.Object.create({id: '2', idx: 1}),
       Ember.Object.create({id: '3', idx: 0})
     ];
@@ -18,10 +20,12 @@ App.IndexController = Ember.ArrayController.extend({
   sortProperties: ['idx'],
 
   updateSortOrder: function(indexes) {
+    this.beginPropertyChanges();
     this.forEach(function(item) {
       var index = indexes[item.get('id')];
       item.set('idx', index);
     }, this);
+    this.endPropertyChanges();
   }
 });
 
